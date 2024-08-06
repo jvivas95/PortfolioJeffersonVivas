bfd19ce8c7d01de8cb8ad9ea1d627baad0e451b8
jvivas95 <137105296+jvivas95@users.noreply.github.com>
2024-08-06 10:33:36 +0200

    Cambio de plantilla

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Mensaje enviado! Nos pondremos en contacto contigo pronto.');
+});
